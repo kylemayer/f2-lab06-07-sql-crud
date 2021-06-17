4b6f45c459ebb16ef38e823a9cf947146c598300
Kyle Mayer <x@kylemayer.dev>
2021-06-17 10:00:27 -0700

    cadded owner_id to ducks js data and tests work!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,89 +5,7 @@ const { execSync } = require('child_process');
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
-
-const ducks = [
-  {
-    _id: '5ff4fb7cd89993a89cc6544f',
-    body_size: 'compact',
-    feet_color: 'orange',
-    id: 1,
-    mass_oz: 14,
-    name: 'Bufflehead Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/bufflehead_ulksab.jpg',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc65444',
-    body_size: 'medium',
-    feet_color: 'brown',
-    id: 2,
-    mass_oz: 20,
-    name: 'Harlequin Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/harlequin_nojjzi.jpg',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc65454',
-    body_size: 'large',
-    feet_color: 'orange',
-    id: 3,
-    mass_oz: 50,
-    name: 'Mallard Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790611/mallard_oukwib.jpg"',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc6545d',
-    body_size: 'medium',
-    feet_color: 'brown',
-    id: 4,
-    mass_oz: 25,
-    name: 'Hooded Merganser Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/merg_gflumv.jpg',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc6545e',
-    body_size: 'giant',
-    feet_color: 'yellow',
-    id: 5,
-    mass_oz: 180,
-    name: 'Muscovy Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790611/muscovy_dh4avw.jpg',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc6546a',
-    body_size: 'medium',
-    feet_color: 'brown',
-    id: 6,
-    mass_oz: 35,
-    name: 'Redhead Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/redhead_wkjbzw.jpg',
-  },
-  {
-    _id: '5ff4fb7cd89993a89cc6546e',
-    body_size: 'compact',
-    feet_color: 'yellow',
-    id: 7,
-    mass_oz: 15,
-    name: 'Wood Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/wood_k9g6zv.jpg',
-  },
-  {
-    _id: '5ff4fb7c83d793a89cc68392',
-    body_size: 'medium',
-    feet_color: 'brown',
-    id: 8,
-    mass_oz: 25,
-    name: 'Mandarin Duck',
-    owner_id: 1,
-    url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623888673/mandarin_gqo1zn.jpg',
-  },
-];
+const ducks = require('../data/ducks.js');
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -129,14 +47,12 @@ describe('app routes', () => {
     test('/GET duck/1 returns a single duck', async() => {
 
       const expectation = {
-        _id: '5ff4fb7cd89993a89cc6544f',
         body_size: 'compact',
         feet_color: 'orange',
         id: 1,
         mass_oz: 14,
         name: 'Bufflehead Duck',
         owner_id: 1,
-        url_image: 'https://res.cloudinary.com/dozx3wxth/image/upload/v1623790612/bufflehead_ulksab.jpg',
       };
 
       const data = await fakeRequest(app)
